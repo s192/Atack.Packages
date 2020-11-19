bc8d48dca877ce9f3142046d87932c429b5dbbb0
s192 <ametaltitan@gmail.com>
2020-11-19 15:01:59 +0800

    完善代码、界面

---
+++ AStack.PdfMerger/MainForm.cs
@@ -0,0 +1,47 @@
+﻿using Atack.WinForm;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AStack.PdfMerger
+{
+    public partial class MainForm : Form
+    {
+        private string[] _names;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowserFileButton_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "PDF(*.pdf)|*.pdf|所有文件(*.*)|*.*";
+            openFileDialog.ShowDialog();
+            var fileNames = openFileDialog.FileNames;
+            if (fileNames == null || fileNames.Length == 0)
+            {
+                return;
+            }
+
+            this.SourceFileListBox.DataSource = fileNames;
+            this.textBox1.Text = fileNames[0];
+            this.TargetPathTextBox.Text = fileNames[0];
+
+            _names = fileNames;
+        }
+
+        private void ApplyButton_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text == this.TargetPathTextBox.Text)
+                return;
+
+            var outMergeFile = this.TargetPathTextBox.Text;
+            PdfHelper.MergePdfFiles(_names.OrderBy(s => s).ToList(), outMergeFile);
+
+            MsgBox.ShowInformation("合并完成！");
+        }
+    }
+}
---
+++ AStack.PdfMerger/PdfHelper.cs
@@ -0,0 +1,30 @@
+﻿using iText.Kernel.Pdf;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AStack.PdfMerger
+{
+    internal static class PdfHelper
+    {
+        /// <summary>          
+        /// 合成pdf文件
+        /// </summary>
+        /// <param name="fileList">文件名list</param>
+        /// <param name="outMergeFile">输出路径</param>
+        public static void MergePdfFiles(List<string> fileList, string outMergeFile)
+        {
+            PdfWriter writer = new PdfWriter(new FileStream(outMergeFile, FileMode.Create));
+            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(writer));
+            var merger = new iText.Kernel.Utils.PdfMerger(pdfDoc);
+            for (int i = 0; i < fileList.Count; i++)
+            {
+                var reader = new PdfReader(fileList[i]);
+                PdfDocument document = new PdfDocument(reader);
+                int iPageNum = document.GetNumberOfPages();
+                merger.Merge(document, 1, iPageNum);
+                document.Close();
+            }
+            pdfDoc.Close();
+        }
+    }
+}
